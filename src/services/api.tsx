1c8439e50d9c3138ca934248201adec08f3d7623
Dhione Castilho Barbosa <dh056256@intelbras.com.br>
2024-11-21 21:36:01 -0300

    feat: update baseUrl onrender

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  //baseURL: 'https://api-pesoperfeito.onrender.com/'
+  baseURL: 'https://api-pesoperfeito.onrender.com/'
   //baseURL: 'http://localhost:3000' // desenvovimento
-   baseURL: 'http://3.147.63.120:3000' // AWS-EC2
+   //baseURL: 'https://3.147.63.120:443' // AWS-EC2
   
  })
 
